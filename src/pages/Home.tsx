7cf062a0e1bf98eb7aebd8ff079d6b5499d32f7f
2ggg <larunaru0386@gmail.com>
2023-04-01 23:17:41 +0900

    CHORE : 초기 세팅 오류 해결

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Home = () => {
+    return (
+        <HomeWrap>
+            <HomeContainer>
+                Home
+            </HomeContainer>
+        </HomeWrap>
+    )
+}
+
+const HomeWrap = styled.div`
+    
+`;
+
+const HomeContainer = styled.div`
+    
+`;
+
+export default Home
