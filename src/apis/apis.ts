deed506ae6be239886c0ce8b6e9dfc74427adccb
yuny0y <yuny0y@naver.com>
2023-04-05 20:36:17 +0900

    DOCS : baseURL 추가

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,1 +1,7 @@
-export { }
+import axios, { AxiosInstance } from 'axios';
+
+export const instance: AxiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+});
+
+export default instance;
