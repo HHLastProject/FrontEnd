a2a8dcfb9c5a854f6b0a79be8325d454ddba4994
yongmin <vmflxu@gmail.com>
2023-04-30 21:52:56 +0900

    FIX: 로그인/비로그인 관련 데이터를 올바르게 가져오지 않는 현상 수정

--- src/hooks/useMypage.ts
+++ src/hooks/useMypage.ts
@@ -1,30 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { mypageKeys } from "../apis/queries";
 import { apiPath } from "../shared/path";
-import { api_token } from "../shared/api";
+import api, { api_token } from "../shared/api";
 import { Feed } from "../pages/Mypage";
+import { useState } from "react";
+import { AxiosError } from "axios";
 
 const useMypage = () => {
 
-    // const typeFixedDispatch = stateDispatch as React.SetStateAction<Feed>;
-
-    const { refetch, isError, isSuccess, data } = useQuery({
+    const { refetch, isError, isSuccess, isLoading, data } = useQuery({
         queryKey: mypageKeys.GET_MYPAGE,
         queryFn: async () => {
+
             const res = await api_token.get(apiPath.mypage);
+            if (res.status === 401) {
+                throw new Error("비로그인");
+            }
+
             const result = res.data.mypages as Feed[];
             return result[0] as Feed;
         },
-        refetchOnMount: true,
-        refetchOnReconnect: true,
-        onSuccess(data) {
-        },
-        onError(err) {
+        onError(err: AxiosError) {
+            if (err.response?.status === 401) {
+                console.log(err);
+                return;
+            }
             throw err;
         }
     });
-
-    return { refetch, isError, isSuccess, data };
+    return { refetch, isError, isSuccess, isLoading, data };
 }
 
 export default useMypage;
